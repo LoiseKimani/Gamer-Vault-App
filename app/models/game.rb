b37bc0bd2f6a2070e281c97362439d7f410e30ed
Loise <loisewakonyo18@gmail.com>
2023-04-02 02:17:03 +0300

    addition of game model data

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,8 @@
 class Game < ApplicationRecord
+  has_many :game_reviews
+  has_many :users, through: :game_reviews
+  validates :title, presence: true, uniqueness: true
+  validates :genre, presence: true
+  validates :release_date, presence: true
 end
+  
