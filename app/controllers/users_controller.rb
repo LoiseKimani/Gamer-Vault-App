81403665c16144dda6f03d9e56c8e27c6d6bb766
Loise <loisewakonyo18@gmail.com>
2023-04-02 01:46:12 +0300

    addition of user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,52 @@
 class UsersController < ApplicationController
+    before_action :require_login, only: [:destroy]
+  
+    def index
+      users = User.all
+      render json: users
+    end
+    
+    def show
+      user = User.find_by(id: params[:id])
+      if user
+        render json: user
+      else
+        render json: { error: "User not found" }, status: :not_found
+      end
+    end
+    
+    def create
+      user = User.new(user_params)
+      
+      if user.save
+        session[:user_id] = user.id
+        render json: user, status: :created
+      else
+        render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+    
+    def destroy
+      user = User.find_by(id: params[:id])
+      if user
+        user.destroy
+        session[:user_id] = nil
+        head :no_content
+      else
+        render json: { error: "User not found" }, status: :not_found
+      end
+    end
+    
+    private
+    
+    def user_params
+      params.require(:user).permit(:username, :email, :password)
+    end
+  
+    def require_login
+      unless session[:user_id]
+        render json: { error: "Not authorized" }, status: :unauthorized
+      end
+    end
 end
+  
