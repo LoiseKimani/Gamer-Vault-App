eb22be8c7ad235738da101ac099a072c5cb459e8
Loise <loisewakonyo18@gmail.com>
2023-04-05 08:17:30 +0300

    minor changes in the client side components

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,17 +1,22 @@
 import React, { useState } from "react";
 
-function Login({ setUser }) {
+function SignUp({ setUser }) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
 
   function handleSubmit(e) {
     e.preventDefault();
-    fetch("/login", {
+    fetch("/signup", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ username, password }),
+      body: JSON.stringify({
+        username,
+        email,
+        password,
+      }),
     }).then((r) => {
       if (r.ok) {
         r.json().then((user) => setUser(user));
@@ -22,7 +27,23 @@ function Login({ setUser }) {
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <h1>Login</h1>
+        <h1>Sign Up</h1>
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          autoComplete="off"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          autoComplete="current-password"
+        />
         <label htmlFor="username">Username</label>
         <input
           type="text"
@@ -31,18 +52,10 @@ function Login({ setUser }) {
           value={username}
           onChange={(e) => setUsername(e.target.value)}
         />
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          id="password"
-          autoComplete="current-password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit">Login</button>
+        <button type="submit">Sign Up</button>
       </form>
     </div>
   );
 }
 
-export default Login;
+export default SignUp;
